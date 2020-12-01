ac56c41dbed8e799dcb9af61381c816a50d31469
Adrian Dominguez <adrian.dominguez1@ibm.com>
2020-12-01 13:47:03 -0600

    insert and update user added

--- src/main/java/com/ibm/adro/learningspringboot/resource/UserResource.java
+++ src/main/java/com/ibm/adro/learningspringboot/resource/UserResource.java
@@ -1,8 +1,9 @@
 package com.ibm.adro.learningspringboot.resource;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+
+import javax.ws.rs.core.MediaType;
 
 import com.ibm.adro.learningspringboot.model.User;
 import com.ibm.adro.learningspringboot.service.UserService;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +48,34 @@ public class UserResource {
         return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorMessage("The User " + userUid + " was not found")));
+    }
+
+    @RequestMapping(
+        method = RequestMethod.POST, 
+        consumes = MediaType.APPLICATION_JSON
+    )
+    public ResponseEntity<Integer> insertNewUser(@RequestBody User user) {
+        int result = userService.insertUser(user); 
+
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        } 
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @RequestMapping(
+        method = RequestMethod.PUT, 
+        consumes = MediaType.APPLICATION_JSON
+    )
+    public ResponseEntity<Integer> updateUser(@RequestBody User user) {
+        int result = userService.updateUser(user); 
+
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        } 
+
+        return ResponseEntity.badRequest().build();
     }
 
     @Data
