bc2d943e284dcfa0341ad50545d551fcb9ed372f
Adrian Dominguez <adrian.dominguez1@ibm.com>
2020-12-01 13:21:26 -0600

    fetchUser

--- src/main/java/com/ibm/adro/learningspringboot/resource/UserResource.java
+++ src/main/java/com/ibm/adro/learningspringboot/resource/UserResource.java
@@ -1,16 +1,22 @@
 package com.ibm.adro.learningspringboot.resource;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.ibm.adro.learningspringboot.model.User;
 import com.ibm.adro.learningspringboot.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @RestController
 @RequestMapping(
@@ -36,8 +42,16 @@ public class UserResource {
         method = RequestMethod.GET, 
         path = "{userUid}"
     )
-    public User fetchUser(@PathVariable("userUid") UUID userUid) {
-        return userService.getUser(userUid).get();
+    public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+        return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorMessage("The User " + userUid + " was not found")));
+    }
+
+    @Data
+    @AllArgsConstructor
+    class ErrorMessage {
+        String ErrorMessage;
     }
 
 }
