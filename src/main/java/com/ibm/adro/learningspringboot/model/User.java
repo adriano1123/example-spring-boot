c42c4605bb0f67af2773a56d1a3c7831f7586fd8
Adrian Dominguez <39064530+adriano1123@users.noreply.github.com>
2020-12-01 13:55:52 -0600

    Test (#1)
    * Resource Added
    
    * vscode added to gitignore
    
    * sync commit
    
    * path added to mappiing
    
    * lombok dependecy added
    
    * fetchUser
    
    * boilerplate code removed from user entity
    
    * no argsConstructor
    
    * insert and update user added
    
    * remove user added to userResources
    
    Co-authored-by: Adrian Dominguez <adrian.dominguez1@ibm.com>

--- src/main/java/com/ibm/adro/learningspringboot/model/User.java
+++ src/main/java/com/ibm/adro/learningspringboot/model/User.java
@@ -2,52 +2,20 @@ package com.ibm.adro.learningspringboot.model;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    private final UUID userUid;
-    private final String firstName;
-    private final String lastName;
-    private final Gender gender;
-    private final Integer age;
-    private final String email;
-
-    public User(UUID userUid, String firstName, String lastName, Gender gender, Integer age, String email) {
-        this.userUid = userUid;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.age = age;
-        this.email = email;
-    }
-
-    public UUID getUserUid() {
-        return userUid;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String toString() {
-        return "User [age=" + age + ", email=" + email + ", firstName=" + firstName + ", gender=" + gender
-                + ", lastName=" + lastName + ", userUid=" + userUid + "]";
-    }
+    private UUID userUid;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private Integer age;
+    private String email;
 
     public enum Gender {
         MALE, FEMALE
---
+++ src/main/java/com/ibm/adro/learningspringboot/resource/UserResource.java
@@ -0,0 +1,101 @@
+package com.ibm.adro.learningspringboot.resource;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.ws.rs.core.MediaType;
+
+import com.ibm.adro.learningspringboot.model.User;
+import com.ibm.adro.learningspringboot.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@RestController
+@RequestMapping(
+    path = "/api/v1/users"
+)
+public class UserResource {
+    
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(
+        method = RequestMethod.GET
+    )
+    public List<User> fetchUsers() {
+        return userService.getAllUsers(); 
+    } 
+
+    @RequestMapping(
+        method = RequestMethod.GET, 
+        path = "{userUid}"
+    )
+    public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+        return userService.getUser(userUid).<ResponseEntity<?>>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorMessage("The User " + userUid + " was not found")));
+    }
+
+    @RequestMapping(
+        method = RequestMethod.POST, 
+        consumes = MediaType.APPLICATION_JSON
+    )
+    public ResponseEntity<Integer> insertNewUser(@RequestBody User user) {
+        int result = userService.insertUser(user); 
+
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        } 
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @RequestMapping(
+        method = RequestMethod.PUT, 
+        consumes = MediaType.APPLICATION_JSON
+    )
+    public ResponseEntity<Integer> updateUser(@RequestBody User user) {
+        int result = userService.updateUser(user); 
+
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        } 
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @RequestMapping(
+        method = RequestMethod.DELETE,
+        path = "{userUid}"
+    )
+    public ResponseEntity<Integer> deleteUser(@PathVariable UUID userUid) {
+        int result = userService.removeUser(userUid); 
+
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        } 
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @Data
+    @AllArgsConstructor
+    class ErrorMessage {
+        String ErrorMessage;
+    }
+
+}
